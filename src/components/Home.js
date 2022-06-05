239c3f8c58558cfd875f9fc448962d0ecb4eb145
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2022-06-05 23:05:37 +0000

    adding authentification form

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        
+    <div className='container mt-5'>
+      <div className="row mb-5">
+          <div className="col-4 offset-8">
+            <p className='budget'>Budget: 0 FCFA</p>
+          </div>
+      </div>
+
+      <button className='mb-5 btn btn-success'>Faire une transaction</button>
+
+      <div className="row">
+        <div className="col-12 col-md-6 col-lg-4 border-end mb-4 mb-lg-none">
+            <h3>Revenu</h3>
+            
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 border-end mb-4 mb-lg-none">
+            <h3>Dépense</h3>
+           
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+            <h3>Informations Transactions</h3>
+            
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,7 +3,34 @@ import React from 'react'
 const Registration = () => {
   return (
     <div>
-        
+        <div className="container mt-5">
+
+            <form className="border p-5">
+                <h2 className="text-center mb-5">Sign up</h2>
+                
+                <div className="mb-5">
+                    <input type="text" className="form-control" placeholder='lastname' />
+                </div>
+
+                <div className="mb-5">
+                    <input type="text" className="form-control" placeholder='Firstname' />
+                </div>
+
+                <div className="mb-5">
+                    <input type="password" className="form-control" placeholder='Username' />
+                </div>
+
+                <div className="mb-5">
+                    <input type="password" className="form-control" placeholder='password' />
+                </div>
+
+                <div className="mb-5 d-flex justify-content-between">
+                    <button type='submit' className='btn btn-success'>Validate</button>
+                    <button className='btn btn-danger'>Cancel</button>
+                </div>
+
+            </form>
+        </div>
     </div>
   )
 }
