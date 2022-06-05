830be54cc82ddd3a48fb1bbdc062e1550614a5ed
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2022-06-05 23:05:37 +0000

    add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container mt-5'>
+        <form className='border p-5'>
+            <h2 className="text-center mb-5">Login</h2>
+
+            <div className="mb-3">
+                <input type="text" className="form-control" placeholder='User name' />
+            </div>
+
+            <div className="mb-3">
+                <input type="password" className="form-control" placeholder='Password' />
+            </div>
+
+            <div className="mb-3 d-flex justify-content-between">
+                <button type="submit" className='btn btn-success'>Validate</button>
+                <button className='btn btn-success'>Sign up</button>
+            </div>
+
+        </form>
+    </div>
   )
 }
 
